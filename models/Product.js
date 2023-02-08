527da8b7e7aecb47afd815a2471fc87dfd14d8ef
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-02-08 15:11:10 -0500

    finish product rest api

---
+++ models/Product.js
@@ -0,0 +1,52 @@
+import Sequelize from "sequelize";
+import db from "../config/dbConfig.js";
+import User from "./User.js";
+
+const Product = db.define(
+  "product",
+  {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    sku: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: {
+        msg: "sku already exists",
+      },
+    },
+    manufacturer: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    owner_user_id: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: "date_added",
+    updatedAt: "date_last_updated",
+  }
+);
+
+export default Product;
