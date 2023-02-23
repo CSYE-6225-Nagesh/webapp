4d48af62028606a5658553a395cb0213664d2bc4
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-02-22 21:29:39 -0500

    complete ami creation workflow

---
+++ app.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sleep 30
+
+sudo yum update -y
+
+#install node version 16
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+
+#Permission for ec2 user
+chmod 755 /home/ec2-user
+
+#Install pm2
+sudo npm install pm2@latest -g
+
+#install mysql mariadb
+sudo yum install mariadb mariadb-server -y
+
+sudo systemctl start mariadb
+
+sudo mysqladmin -u root password $DB_PASSWORD
+
+mysqladmin -u root --password=nagesh1610 --host=localhost --port=3306 create csye_database
+
+sudo systemctl enable mariadb
+
+# install unzip
+sudo yum install unzip -y
+
+#Get inside the application
+cd ~/ && unzip webapp.zip
+cd ~/webapp && npm ci
+
+sudo mkdir -p ~/logs
+#Start app using pm2
+sudo pm2 startup systemd --service-name webapp
+sudo pm2 start ecosystem.config.cjs
+sudo pm2 save
+sudo pm2 list
