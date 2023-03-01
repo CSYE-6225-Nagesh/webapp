d9c81728d6434a13af42c5acc6681640a7eabe53
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-03-01 18:59:36 -0500

    complete image upload

---
+++ controller/imageController.js
@@ -0,0 +1,101 @@
+import Image from "../models/Image.js";
+import Product from "../models/Product.js";
+import { Op } from "sequelize";
+import { v4 as uuidv4 } from "uuid";
+import { uploadFile, deleteFile } from "../utils/s3.js";
+
+export const getAllImages = async (req, res) => {
+  console.log("getAllImages");
+  try {
+    const images = await Image.findAll({
+      where: {
+        [Op.and]: [{ product_id: req.params.productId }],
+      },
+    });
+    if (images.length > 0) {
+      res.status(200).json(images);
+    } else {
+      return res.status(404).json({ message: "No images found for product" });
+    }
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+};
+
+export const getImage = async (req, res) => {
+  console.log("get image has been hit");
+  try {
+    const image = await Image.findOne({
+      where: {
+        [Op.and]: [
+          { product_id: req.params.productId },
+          { image_id: req.params.imageId },
+        ],
+      },
+    });
+    if (image) {
+      res.status(200).json(image);
+    } else {
+      return res.status(404).json({ message: "No such image" });
+    }
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+};
+
+export const deleteImage = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: { id: req.params.productId },
+    });
+    if (product.owner_user_id == req.user.id) {
+      const image = await Image.findOne({
+        where: {
+          [Op.and]: [
+            { product_id: req.params.productId },
+            { image_id: req.params.imageId },
+          ],
+        },
+      });
+
+      if (image) {
+        await deleteFile(image.file_name);
+        Image.destroy({ where: { image_id: req.params.imageId } }).then(
+          (data) => {
+            return res.status(204).json(data);
+          }
+        );
+      } else {
+        return res.status(404).json({ message: "Image not found" });
+      }
+    } else {
+      return res.status(403).json({ message: "Delete image forbidden" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const addImage = async (req, res) => {
+  const fileName = `${uuidv4()} ${req.file.originalname}`;
+  const fileBuffer = req.file.fileBuffer;
+  try {
+    const product = await Product.findOne({
+      where: { id: req.params.productId },
+    });
+    if (product.owner_user_id == req.user.id) {
+      await uploadFile(fileBuffer, fileName, req.file.mimetype);
+      const image = {
+        product_id: req.params.productId,
+        file_name: fileName,
+        s3_bucket_path: `${process.env.AWS_BUCKET_NAME}/${fileName}`,
+      };
+      const data = await Image.create(image);
+      res.status(201).json(data);
+    } else {
+      res.status(403).json({ message: "forbidden" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
