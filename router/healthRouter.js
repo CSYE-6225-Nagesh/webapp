f95a25065198ac2f51a83880e1d25983e156bcdf
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-04-13 11:09:33 -0400

    add health endpoint

--- router/healthRouter.js
+++ router/healthRouter.js
@@ -4,4 +4,6 @@ import checkHealth from "../controller/healthCheckController.js";
 const router = new Router();
 router.route("/healthz").get(checkHealth);
 
+router.route("/health").get(checkHealth);
+
 export default router;
